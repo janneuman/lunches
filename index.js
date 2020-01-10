4f40fc9f169956002353890e4237af75792590c5
Jan Neuman <neuman.jan@gmail.com>
2020-01-10 19:38:32 +0100

    try routes

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ import {
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(express.static(path.join(__dirname, 'build')))
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/ecko', async (req, res) => {
   res.send(await getEckoKantyna());
