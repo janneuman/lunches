9b34191a4975ae47c2d0e840e67518a25cf98e38
Jan Neuman <neuman.jan@gmail.com>
2020-01-10 11:38:59 +0100

    fix async await

--- restaurants/eckoKantyna.js
+++ restaurants/eckoKantyna.js
@@ -2,10 +2,13 @@ import request from "request";
 import cheerio from "cheerio";
 
 export const getEckoKantyna = async () => {
-  const html = await request.get('https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/', (error, response, body) => {
+  const html = await request.get(
+    'https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/',
+    (error, response, body) => {
+      console.log(body, 'body')
     const $ = cheerio.load(body);
     return $('#hlavni .cely').html();
   });
-  
+// console.log(html, 'html')
   return html;
 };
