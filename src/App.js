7b47909824054df717c77cd3e69e29a64f402c02
Jan Neuman <neuman.jan@gmail.com>
2020-01-10 15:54:06 +0100

    add FE framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Pane, Tablist, Tab, Paragraph } from 'evergreen-ui';
 
 function App() {
+  const [tabIndex, setTabIndex] = React.useState(0);
+  const [tabs, setTabs] = React.useState(['Traits', 'Event History', 'Identities']);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Pane height={120}>
+      <Tablist marginBottom={16} flexBasis={240} marginRight={24}>
+        {tabs.map((tab, index) => (
+          <Tab
+            key={tab}
+            id={tab}
+            onSelect={() => setTabIndex(index)}
+            isSelected={index === tabIndex}
+            aria-controls={`panel-${tab}`}
+          >
+            {tab}
+          </Tab>
+        ))}
+      </Tablist>
+      <Pane padding={16} background="tint1" flex="1">
+        {tabs.map((tab, index) => (
+          <Pane
+            key={tab}
+            id={`panel-${tab}`}
+            role="tabpanel"
+            aria-labelledby={tab}
+            aria-hidden={index !== tabIndex}
+            display={index === tabIndex ? 'block' : 'none'}
+          >
+            <Paragraph>Panel {tab}</Paragraph>
+          </Pane>
+        ))}
+      </Pane>
+    </Pane>
   );
 }
 
