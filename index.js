de66653ad6c130f90d7a36a8dc8ae6573201b181
Jan Neuman <neuman.jan@gmail.com>
2020-01-10 16:26:36 +0100

    add awesome first restaurant

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'build')))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
--- src/App.js
+++ src/App.js
@@ -1,38 +1,70 @@
 import * as React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Pane, Tablist, Tab, Paragraph } from 'evergreen-ui';
+import { useEffect } from 'react';
+
+const getMenu = async url => {
+  const data = await fetch(`http://localhost:3000${url}`);
+  return await data.text();
+};
 
 function App() {
   const [tabIndex, setTabIndex] = React.useState(0);
-  const [tabs, setTabs] = React.useState(['Traits', 'Event History', 'Identities']);
+  const [tabs, setTabs] = React.useState([
+    {
+      name: 'Kantyna Ecko',
+      url: '/ecko',
+      content: '',
+    },
+  ]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const content = await getMenu(tabs[tabIndex].url);
+
+      const newTabs = tabs.map((tab, index) => {
+        if (index === tabIndex) {
+          return {
+            ...tab,
+            content,
+          };
+        }
+        return tab;
+      });
+      setTabs(newTabs);
+    }
+    fetchData();
+
+  }, [tabIndex]);
 
   return (
     <Pane height={120}>
       <Tablist marginBottom={16} flexBasis={240} marginRight={24}>
         {tabs.map((tab, index) => (
           <Tab
-            key={tab}
-            id={tab}
-            onSelect={() => setTabIndex(index)}
+            key={tab.name}
+            id={tab.name}
+            onSelect={() => {
+              setTabIndex(index);
+            }}
             isSelected={index === tabIndex}
-            aria-controls={`panel-${tab}`}
+            aria-controls={`panel-${tab.name}`}
           >
-            {tab}
+            {tab.name}
           </Tab>
         ))}
       </Tablist>
       <Pane padding={16} background="tint1" flex="1">
         {tabs.map((tab, index) => (
           <Pane
-            key={tab}
-            id={`panel-${tab}`}
+            key={tab.name}
+            id={`panel-${tab.name}`}
             role="tabpanel"
-            aria-labelledby={tab}
+            aria-labelledby={tab.name}
             aria-hidden={index !== tabIndex}
             display={index === tabIndex ? 'block' : 'none'}
           >
-            <Paragraph>Panel {tab}</Paragraph>
+            <div dangerouslySetInnerHTML={{__html: tab.content}} />
           </Pane>
         ))}
       </Pane>
