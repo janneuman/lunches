7c744ae993a8cc59376c187f511e0588ba165ae0
Jan Neuman <neuman.jan@gmail.com>
2020-01-10 13:17:09 +0100

    WIP

--- restaurants/eckoKantyna.js
+++ restaurants/eckoKantyna.js
@@ -1,14 +1,39 @@
 import request from "request";
 import cheerio from "cheerio";
+import fetch from "node-fetch";
 
 export const getEckoKantyna = async () => {
-  const html = await request.get(
-    'https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/',
-    (error, response, body) => {
-      console.log(body, 'body')
-    const $ = cheerio.load(body);
-    return $('#hlavni .cely').html();
-  });
-// console.log(html, 'html')
-  return html;
+  // console.log('aaaa')
+  // const getData = async () => await fetch('https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/')
+  // const response = await getPage()
+  // await request.get(
+  //   'https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/',
+  //   (error, response, body) => {
+  //   const $ = cheerio.load(body);
+  //   const a = $('.cely').html();
+  //   console.log(a, '_________gggg');
+  //   return a;
+  // });
+
+
+
+  const url = "https://www.prague-catering.cz/provozovny/Kantyna-ECKO/Denni-menu-kantyny-ECKO/";
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.text();
+      console.log(json, 'QQQQQQQQ');
+      return json
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const page = await getData(url);
+  console.log(page, '------page')
+  const $ = cheerio.load(page);
+  const a = $('.cely').html();
+  console.log(a, '------a')
+  return a;
 };
+
+
--- test.js
+++ test.js
@@ -2,5 +2,7 @@ import { getPivoKarlin } from './restaurants/pivoKarlin';
 import { getEckoKantyna } from './restaurants/eckoKantyna';
 
 export const test = async (req, res) => {
-  res.send(getEckoKantyna())
+  const s = await getEckoKantyna();
+  console.log('asss')
+  res.send(s);
 };
