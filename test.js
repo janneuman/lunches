ed7e00e280a54e38067d14c21440aea7ba907da5
Jan Neuman <neuman.jan@gmail.com>
2020-01-11 14:36:19 +0100

    remove unused files

--- test.js
+++
@@ -1,8 +0,0 @@
-import { getPivoKarlin } from './restaurants/pivoKarlin';
-import { getEckoKantyna } from './restaurants/eckoKantyna';
-
-export const test = async (req, res) => {
-  const s = await getEckoKantyna();
-  console.log('asss')
-  res.send(s);
-};
